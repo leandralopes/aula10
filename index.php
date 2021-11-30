74cd8e10004a22875c501b1aa23d8aab4fad0442
Leandra Lopes <leandralopes477@gmail.com>
2021-11-29 21:18:20 -0300

    versao 0.0.9

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Controle</title>
+</head>
+<body>
+    Menu Principal
+    <hr>
+    <a href="frmclientes.php">Clientes</a> - 
+    <a href="produtos.php">Produtos</a> -
+    <a href="vendedores.php">Vendedores</a> - 
+    <a href="vendas.php">Vendas</a> -
+
+</body>
+</html>
